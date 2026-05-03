1397296bc84aaf017f7bf63b760321b067e85eb1
YashVaswani <yashvaswani2165@gmail.com>
2026-05-03 12:00:00 +0530

    Complete SafeNest Implementation

--- mobile/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mobile/main.dart';
-
-void main() {
-  testWidgets('App launches smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const SafeNestApp());
-    await tester.pump(const Duration(seconds: 1));
-  });
-}
